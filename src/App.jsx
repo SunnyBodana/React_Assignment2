38dedea892e8a7d3273727a6a86da78678977d4a
SunnyBodana <sunnybodana941@gmail.com>
2025-06-28 16:39:23 +0530

    List

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import Component1 from './Component1';
-import Jsxcomponents from './Jsxcomponents';
-import Greetingthat from './Greetingthat';
-import WelcomeMessagethat from './WelcomeMessagethat';
-import UserCardthat from './UserCardthat';
-import Countercomponent from './Countercomponent'
+// import Five from './Five';
+// import Second from './Second';
+// import Third from './Third';
+
 
 function App() {
   return (
-    <>
-      {/* <Component1></Component1> */}
-      {/* <Jsxcomponents></Jsxcomponents> */}
-      {/* <Greetingthat name = {"I Am Mern Stack Developer"}></Greetingthat> */}
-      {/* <WelcomeMessagethat></WelcomeMessagethat> */}
-      {/* <UserCardthat name={"JOHN DOE"} age="30" location={'USA'}></UserCardthat> */}
-      {/* <Countercomponent></Countercomponent> */}
-    </>
+    <div className='App'>
+     {/* <First></First> */}
+      {/* <Second></Second> */}
+      {/* <Third></Third> */}
+      {/* <Five></Five> */}
+     
+     
+    </div>
   );
 }
 
